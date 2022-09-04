2a1f1254fa5c623b29549b1413a053315b8c7eba
Nikita <Solovev.nikita.05@gmail.com>
2022-09-04 23:45:25 +0300

    rename random_word function to get_random_word. Completed doc string

---
+++ bot/generate_next_random_word.py
@@ -0,0 +1,12 @@
+import telegram.ext
+import random
+
+
+def get_random_word(update: telegram.Update, context: telegram.ext.CallbackContext):
+    """
+    Generates a random word from 'context.user_data['learning_words']'
+    and writes it to context.user_data['ran_num']
+    """
+    context.user_data['ran_num'] = random.randint(0, 2)  # Записывает число от 0 до 2 в context.user_data['ran_num']
+    update.message.reply_text(text=f'{context.user_data["learning_words"][context.user_data["ran_num"]]}')
+    # Отправляет сообщение со случайно выбранным словом.
