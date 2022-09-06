92a2525ea56dd50402bdf249409168a852271f8a
Nikita <Solovev.nikita.05@gmail.com>
2022-09-06 13:58:22 +0300

    Readability

--- bot/start_command.py
+++ bot/start_command.py
@@ -3,8 +3,9 @@ import telegram.ext
 
 def start_command(update: telegram.Update, context: telegram.ext.CallbackContext):
     """When executing the command '/start' informs the user"""
-    update.message.reply_text(text=f'Привет {update.message.from_user.first_name}! '
-                                   f'Меня зовут {update.message.bot.first_name}.\n'
-                                   f'Я буду помогать тебе изучать иностранные слова.\n'
-                                   f'Сперва, напиши команду /add, что бы добавить слова.'
-                              )
+    update.message.reply_text(
+        text=f'Привет {update.message.from_user.first_name}! '
+             f'Меня зовут {update.message.bot.first_name}.\n'
+             f'Я буду помогать тебе изучать иностранные слова.\n'
+             f'Сперва, напиши команду /add, что бы добавить слова.'
+    )
