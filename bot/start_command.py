695ba30d5ca6474abe0b4f0360bf323415d8c3eb
Nikita <Solovev.nikita.05@gmail.com>
2022-09-06 13:53:08 +0300

    Move functions to get_learning_words.py. Change start_command function text-information.

--- bot/start_command.py
+++ bot/start_command.py
@@ -1,36 +1,10 @@
 import telegram.ext
-import configurations.settings
 
 
 def start_command(update: telegram.Update, context: telegram.ext.CallbackContext):
-    """When executing the command '/start' introduces the user"""
-    update.message.reply_text(text=f'Привет! Меня зовут {update.message.bot.first_name}.\n'
-                                   f'Сперва, тебе нужно добавить {configurations.settings.NUMBER_OF_WORDS}'
-                                   f' {start_command_spellings()}.\n'  # В зависимости от числа слов меняет написание.
-                                   f'Пример: {create_example_words()}\n'
+    """When executing the command '/start' informs the user"""
+    update.message.reply_text(text=f'Привет {update.message.from_user.first_name}! '
+                                   f'Меня зовут {update.message.bot.first_name}.\n'
+                                   f'Я буду помогать тебе изучать иностранные слова.\n'
+                                   f'Сперва, напиши команду /add, что бы добавить слова.'
                               )
-
-
-def start_command_spellings():
-    """
-    Changes the spelling of start_command introduce
-    depending on the configurations.settings.NUMBER_OF_WORDS
-    """
-    if 5 > configurations.settings.NUMBER_OF_WORDS > 1:
-        spelling = 'изучаемых слова'
-    elif configurations.settings.NUMBER_OF_WORDS == 1:
-        spelling = 'изучаемое слово'
-    else:
-        spelling = 'изучаемых слов'
-    return spelling
-
-
-def create_example_words():
-    """
-    Creates the number of words equal to configurations.settings.NUMBER_OF_WORDS
-     for (f'Пример: {create_example_words()}\n') in start_command functions
-    """
-    words = '/add '
-    for i in range(configurations.settings.NUMBER_OF_WORDS):
-        words += f'word{i + 1} '
-    return words
