37f63e0f037a642e68ea1e53482e83285cdbd0f9
Nikita <Solovev.nikita.05@gmail.com>
2022-09-08 21:07:56 +0300

    Readability

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ from configurations.settings import TOKEN
 
 
 def main():
-    print('**START**')
+    print('***START***')
     updater = telegram.ext.Updater(token=TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(telegram.ext.CommandHandler(command='start', callback=bot.start_command.start_command))
     dp.add_handler(telegram.ext.ConversationHandler(
         entry_points=[telegram.ext.CommandHandler('add', bot.get_learning_words.asks_for_words, pass_user_data=True), ],
         states={
-            bot.get_learning_words.WORDS: [
+            bot.get_learning_words.GET_ENTERED_WORDS: [
                 telegram.ext.MessageHandler(
                     filters=telegram.ext.Filters.text & (~ telegram.ext.Filters.command),
                     callback=bot.get_learning_words.get_learning_words,
                     pass_user_data=True),
             ],
-            bot.get_learning_words.GET_WORDS: [
+            bot.get_learning_words.TRANSLATE_ENTERED_WORDS: [
                 telegram.ext.MessageHandler(
                     filters=telegram.ext.Filters.text & (~ telegram.ext.Filters.command),
-                    callback=bot.user_response_actions.get_user_answer,
+                    callback=bot.user_response_actions.get_translated_word,
                     pass_user_data=True),
             ],
         },
-        fallbacks=[telegram.ext.CommandHandler('stop', bot.get_learning_words.stop_guessing)],
+        fallbacks=[telegram.ext.CommandHandler('stop', bot.get_learning_words.stop_conversation)],
     )
     )
     updater.start_polling()
