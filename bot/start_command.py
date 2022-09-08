28299b42f16a7736e3e6baba5d60e1555886ec3b
Nikita <Solovev.nikita.05@gmail.com>
2022-09-08 21:06:59 +0300

    Update message text

--- bot/start_command.py
+++ bot/start_command.py
@@ -2,10 +2,10 @@ import telegram.ext
 
 
 def start_command(update: telegram.Update, context: telegram.ext.CallbackContext):
-    """When executing the command '/start' informs the user"""
+    """Send a message that informs the user"""
     update.message.reply_text(
-        text=f'Привет {update.message.from_user.first_name}! '
-             f'Меня зовут {update.message.bot.first_name}.\n'
+        text=f'Привет {update.message.from_user.first_name}! Меня зовут {update.message.bot.first_name}.\n'
              f'Я буду помогать тебе изучать иностранные слова.\n'
-             f'Сперва, напиши команду /add, что бы добавить слова.'
+             f'Сперва, напиши команду /add, что бы добавить слова.\n'
+             f'Язык написанных вами слов определяется автоматически.'
     )
