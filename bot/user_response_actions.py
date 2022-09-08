75b1a6f188b2d8e69acb70d454eed205e02ad360
Nikita <Solovev.nikita.05@gmail.com>
2022-09-08 21:07:19 +0300

    Simplification

--- bot/user_response_actions.py
+++ bot/user_response_actions.py
@@ -1,30 +1,18 @@
 import telegram.ext
-import translators
 import bot
-import configurations.settings
 
 
-def get_user_answer(update: telegram.Update, context: telegram.ext.CallbackContext):
-    """Gets the user's response to the given word from get_random_word function"""
-    text = update.message.text  # Ответ пользователя на предоставленное ему слово
-    word = context.user_data["learning_words"][context.bot_data["ran_num"]]  # Достаёт сгенерированное слово из
-    # функции random_num при помощи индекса context.bot_data["ran_num"]
-    get_translated_word(update=update, context=context, text=text, word=word)
+def get_translated_word(update: telegram.Update, context: telegram.ext.CallbackContext):
+    """Gets the translated word from context.user_data['learning_words_translated']"""
+    translated_word = context.user_data['learning_words_translated'][context.bot_data["ran_num"]]  # Достает
+    # переведенное слово из context.user_data['learning_words_translated'].
+    check_answer_correctness(update=update, context=context, translated_word=translated_word)
 
 
-def get_translated_word(update: telegram.Update, context: telegram.ext.CallbackContext, text, word):
-    """Translates the word variable derived from the get_user_answer"""
-    translated_word = translators.google(
-        query_text=word,
-        from_language=configurations.settings.FROM_LANGUAGE,
-        to_language=configurations.settings.TO_LANGUAGE
-    )  # переведённая переменная word
-    check_answer_correctness(update=update, context=context, text=text, translated_word=translated_word)
-
-
-def check_answer_correctness(update: telegram.Update, context: telegram.ext.CallbackContext, text, translated_word):
+def check_answer_correctness(update: telegram.Update, context: telegram.ext.CallbackContext, translated_word):
     """Checks the translated word entered by the user for correctness"""
-    if text.lower() == translated_word.lower():
+    answer = update.message.text
+    if answer.lower() == translated_word.lower():
         update.message.reply_text(text='Правильно!')
         bot.get_random_word(update=update, context=context)  # Вызов функции random_word для зацикливания.
     else:
