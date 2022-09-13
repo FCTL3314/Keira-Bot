7f3c2d630baeab1344bc75b01d6d8a8a16d17592
Nikita <Solovev.nikita.05@gmail.com>
2022-09-13 21:31:20 +0300

    Text edit

--- bot/unexpected_messages_reply.py
+++ bot/unexpected_messages_reply.py
@@ -2,5 +2,5 @@ import telegram.ext
 
 
 def unexpected_message_reply(update: telegram.Update, context: telegram.ext.CallbackContext):
-    update.message.reply_text(text='Моей программой не предусмотрена предоставленная вами смысловая нагрузка. '
+    update.message.reply_text(text='Моей программой не предусмотрена предоставленная вами смысловая нагрузка.\n'
                                    'Пожалуйста, напишите то, что я понимаю :3')
