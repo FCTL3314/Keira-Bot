63896f515f3ea943d53fa8b3415e5d62bd67f5e7
Nikita <Solovev.nikita.05@gmail.com>
2022-09-25 00:01:36 +0300

    Now, for the word of each user, a separate word is created based on the chat ID

--- bot/generate_next_random_word.py
+++ bot/generate_next_random_word.py
@@ -5,6 +5,7 @@ import configurations.config
 
 def get_random_word(update: telegram.Update, context: telegram.ext.CallbackContext):
     """Generates a random word and send it"""
-    context.bot_data['ran_num'] = random.randint(0, (configurations.config.NUMBER_OF_WORDS - 1))
+    context.bot_data[f"ran_num - {update.message.chat_id}"] = random.randint(0, (
+                configurations.config.NUMBER_OF_WORDS - 1))
     update.message.reply_text(
-        text=f'{context.user_data["learning_words"][context.bot_data["ran_num"]]}')
+        text=f'{context.user_data["learning_words"][context.bot_data[f"ran_num - {update.message.chat_id}"]]}')
