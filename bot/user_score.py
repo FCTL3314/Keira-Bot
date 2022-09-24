1b17632fadbd44a0ad79f5675255f89243f15339
Nikita <Solovev.nikita.05@gmail.com>
2022-09-24 18:42:35 +0300

    create Score class

---
+++ bot/user_score.py
@@ -0,0 +1,15 @@
+class Score:
+    def __init__(self):
+        self._score = 0
+
+    def increment(self):
+        self._score += 1
+
+    def reset(self):
+        self._score = 0
+
+    def get_score(self):
+        return self._score
+
+
+score = Score()
