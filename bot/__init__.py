60324196f227bcf6d2de463fe37431fd6500bdbf
Nikita <Solovev.nikita.05@gmail.com>
2022-10-03 22:59:32 +0300

    db_actions import

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,6 @@
 from .generate_next_random_word import generate_random_word
 from .create_keyboard_markup import create_keyboard_markup
 from .user_score import UserScore
+from .db_actions import db_write_last_learning_words, db_get_last_learning_words
 
 GET_ENTERED_WORDS, TRANSLATE_ENTERED_WORDS = range(2)
