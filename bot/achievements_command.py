f52b856d2a6c1e9317479487d4feb954c85ccf7a
Nikita <Solovev.nikita.05@gmail.com>
2022-10-04 19:56:09 +0300

    create send achievements/stats message function

---
+++ bot/achievements_command.py
@@ -0,0 +1,10 @@
+import telegram.ext
+import connectors.db_actions
+
+
+def send_achievements_message(update: telegram.Update, context: telegram.ext.CallbackContext):
+    learned_words = connectors.db_actions.db_get_learned_words(update=update)
+    best_score = connectors.db_actions.db_get_best_score(update=update)
+    update.message.reply_text(text=f'Кол-во выученных тобою слов - {len(learned_words)}\n'
+                                   f'А вот и библиотека твоих слов: \n{", ".join(learned_words)}.')
+    update.message.reply_text(text=f'Твоя лучшая серия правильных ответов - {best_score}')
