745ba0ac634bae882ac4adc75eb6b61526f7b76e
Nikita <Solovev.nikita.05@gmail.com>
2022-10-04 23:57:18 +0300

    None type check

--- bot/achievements_command.py
+++ bot/achievements_command.py
@@ -5,6 +5,9 @@ import connectors.db_actions
 def send_achievements_message(update: telegram.Update, context: telegram.ext.CallbackContext):
     learned_words = connectors.db_actions.db_get_learned_words(update=update)
     best_score = connectors.db_actions.db_get_best_score(update=update)
-    update.message.reply_text(text=f'Кол-во выученных тобою слов - {len(learned_words)}\n'
-                                   f'А вот и библиотека твоих слов: \n{", ".join(learned_words)}.')
-    update.message.reply_text(text=f'Твоя лучшая серия правильных ответов - {best_score}')
+    if learned_words is None:
+        update.message.reply_text(text='Для отображения статистики тебе необходимо выучить свои первые слова.')
+    else:
+        update.message.reply_text(text=f'Кол-во выученных тобою слов - {len(learned_words)}\n'
+                                       f'А вот и библиотека твоих слов: \n{", ".join(learned_words)}.')
+        update.message.reply_text(text=f'Твоя лучшая серия правильных ответов - {best_score}')
