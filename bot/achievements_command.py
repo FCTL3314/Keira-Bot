2b5fe5321c22de0851ea344ab3161eb0c0a9a804
Nikita <Solovev.nikita.05@gmail.com>
2022-10-05 13:48:12 +0300

    message text edit

--- bot/achievements_command.py
+++ bot/achievements_command.py
@@ -6,7 +6,8 @@ def send_achievements_message(update: telegram.Update, context: telegram.ext.Cal
     learned_words = connectors.db_actions.db_get_learned_words(update=update)
     best_score = connectors.db_actions.db_get_best_score(update=update)
     if learned_words is None:
-        update.message.reply_text(text='Для отображения статистики тебе необходимо выучить свои первые слова.')
+        update.message.reply_text(text=f'Ты ещё не выучил ни одного слова.\n'
+                                       f'Твоя лучшая серия правильных ответов - {best_score}')
     else:
         update.message.reply_text(text=f'Кол-во выученных тобою слов - {len(learned_words)}\n'
                                        f'А вот и библиотека твоих слов: \n{", ".join(learned_words)}.')
