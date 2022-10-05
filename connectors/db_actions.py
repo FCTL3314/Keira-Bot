c78306923c84edcb53016fe59a443a48a3d6f794
Nikita <Solovev.nikita.05@gmail.com>
2022-10-05 20:23:58 +0300

    type annotation added

--- connectors/db_actions.py
+++ connectors/db_actions.py
@@ -1,5 +1,6 @@
 import sqlite3
 import telegram.ext
+from typing import List
 
 
 def db_create_user_info(update: telegram.Update):
@@ -10,7 +11,7 @@ def db_create_user_info(update: telegram.Update):
             cur.execute(f'INSERT INTO user_data (user_id) VALUES ({update.message.from_user.id})')
 
 
-def db_add_learned_words(learned_words, update: telegram.Update):
+def db_add_learned_words(learned_words: List[str], update: telegram.Update):
     with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
         cur = conn.cursor()
         cur.execute(f'SELECT user_id FROM user_data WHERE user_id == "{update.message.from_user.id}"')
@@ -18,7 +19,7 @@ def db_add_learned_words(learned_words, update: telegram.Update):
                     f'WHERE user_id = {update.message.from_user.id}')
 
 
-def db_update_best_score(score, update: telegram.Update):
+def db_update_best_score(score: int, update: telegram.Update):
     with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
         cur = conn.cursor()
         cur.execute(f'SELECT best_score FROM user_data WHERE user_id == "{update.message.from_user.id}"')
@@ -26,7 +27,7 @@ def db_update_best_score(score, update: telegram.Update):
             cur.execute(f'UPDATE user_data SET best_score = {score} WHERE user_id == "{update.message.from_user.id}"')
 
 
-def db_get_learned_words(update: telegram.Update):
+def db_get_learned_words(update: telegram.Update) -> List[str]:
     with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
         db_create_user_info(update=update)
         cur = conn.cursor()
@@ -36,7 +37,7 @@ def db_get_learned_words(update: telegram.Update):
             return cur.fetchone()[0].split()
 
 
-def db_get_best_score(update: telegram.Update):
+def db_get_best_score(update: telegram.Update) -> int:
     with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
         cur = conn.cursor()
         cur.execute(f'SELECT best_score FROM user_data WHERE user_id == {update.message.from_user.id}')
