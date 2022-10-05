1a359a4594e58e4250b6bcc10c5e947018deb47d
Nikita <Solovev.nikita.05@gmail.com>
2022-10-05 20:13:20 +0300

    updated the text of messages

--- bot/achievements_command.py
+++ bot/achievements_command.py
@@ -6,9 +6,11 @@ def send_achievements_message(update: telegram.Update, context: telegram.ext.Cal
     learned_words = connectors.db_actions.db_get_learned_words(update=update)
     best_score = connectors.db_actions.db_get_best_score(update=update)
     if learned_words is None:
-        update.message.reply_text(text=f'Ты ещё не выучил ни одного слова.\n'
-                                       f'Твоя лучшая серия правильных ответов - {best_score}')
+        update.message.reply_text(text=f'Что-то я не могу найти слов в твоей библиотеке...')
+        if best_score == 0:
+            update.message.reply_text(text=f'Твой лучший счёт так же пуст.')
+        else:
+            update.message.reply_text(text=f'Однако твой лучший счёт - {best_score}')
     else:
-        update.message.reply_text(text=f'Кол-во выученных тобою слов - {len(learned_words)}\n'
-                                       f'А вот и библиотека твоих слов: \n{", ".join(learned_words)}.')
+        update.message.reply_text(text=f'Библиотека твоих слов:\n{", ".join(learned_words)}.')
         update.message.reply_text(text=f'Твоя лучшая серия правильных ответов - {best_score}')
