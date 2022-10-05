b262274fc1128fab7da6dc2fb7137752fedee767
Nikita <Solovev.nikita.05@gmail.com>
2022-10-05 21:59:04 +0300

    db_add_learned_words now concat old and new words and writes it as new.

--- connectors/db_actions.py
+++ connectors/db_actions.py
@@ -12,20 +12,24 @@ def db_create_user_info(update: telegram.Update):
             cur.execute(f'INSERT INTO user_data (user_id) VALUES ({update.message.from_user.id})')
 
 
-def db_add_learned_words(learned_words: List[str], update: telegram.Update):
+def db_add_learned_words(learned_words: set, update: telegram.Update):
     with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
         cur = conn.cursor()
-        cur.execute(f'SELECT user_id FROM user_data WHERE user_id == "{update.message.from_user.id}"')
-        cur.execute(f'UPDATE user_data SET learned_words = "{" ".join(learned_words)}" '
-                    f'WHERE user_id = {update.message.from_user.id}')
+        cur.execute(f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
+        if cur.fetchone()[0] is None:
+            cur.execute(f'UPDATE user_data SET learned_words = '
+                        f'"{" ".join(learned_words)}" WHERE user_id = {update.message.from_user.id}')
+        else:
+            cur.execute(f'UPDATE user_data SET learned_words = '
+                        f'learned_words || "{" ".join(learned_words)}" WHERE user_id = {update.message.from_user.id}')
 
 
 def db_update_best_score(score: int, update: telegram.Update):
     with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
         cur = conn.cursor()
-        cur.execute(f'SELECT best_score FROM user_data WHERE user_id == "{update.message.from_user.id}"')
+        cur.execute(f'SELECT best_score FROM user_data WHERE user_id == {update.message.from_user.id}')
         if cur.fetchone()[0] < score:
-            cur.execute(f'UPDATE user_data SET best_score = {score} WHERE user_id == "{update.message.from_user.id}"')
+            cur.execute(f'UPDATE user_data SET best_score = {score} WHERE user_id == {update.message.from_user.id}')
 
 
 def db_get_learned_words(update: telegram.Update) -> List[str]:
