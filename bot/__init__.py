ecb14b6472ebf898d89fd592fd5d955efb99694c
Nikita <Solovev.nikita.05@gmail.com>
2022-10-06 22:11:13 +0300

    import function rename

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,4 +1,4 @@
-from .generate_next_random_word import send_random_word
+from .send_random_word import send_random_word
 from .create_keyboard_markup import create_keyboard_markup
 from .user_score import UserScore
 
