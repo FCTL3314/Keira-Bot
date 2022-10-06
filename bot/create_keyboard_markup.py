a5e7416e4610cb415e8ad4796fdab6ebdc265253
Nikita <Solovev.nikita.05@gmail.com>
2022-10-06 19:31:42 +0300

    function simplification

--- bot/create_keyboard_markup.py
+++ bot/create_keyboard_markup.py
@@ -1,7 +1,5 @@
 import telegram.ext
 
 
-def create_keyboard_markup(context: telegram.ext.CallbackContext):
-    translated_words = context.user_data['learning_words_translated']
-    return telegram.ReplyKeyboardMarkup([[translated_words[i]] for i in range(len(translated_words))],
-                                        one_time_keyboard=False)
+def create_keyboard_markup(translated_words):
+    return telegram.ReplyKeyboardMarkup([[translated_words[i]] for i in range(len(translated_words))])
