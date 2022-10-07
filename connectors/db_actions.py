60e68e3de3e4d829031ca0d85ee7b5fd0e48cf82
Nikita <Solovev.nikita.05@gmail.com>
2022-10-07 11:30:17 +0300

    unite db functions into class, refactor methods for simplification, readability

--- connectors/db_actions.py
+++ connectors/db_actions.py
@@ -4,46 +4,55 @@ import telegram.ext
 from typing import List
 
 
-def db_create_user_info(update: telegram.Update):
-    with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
-        cur = conn.cursor()
-        cur.execute(f'SELECT user_id FROM user_data WHERE user_id == "{update.message.from_user.id}"')
-        if cur.fetchone() is None:
-            cur.execute(f'INSERT INTO user_data (user_id) VALUES ({update.message.from_user.id})')
+class DataBase:
+    def __init__(self, route: str):
+        self.__conn = sqlite3.connect(route, check_same_thread=False)
+        self.__cur = self.__conn.cursor()
+
+    def find_user_id_record(self, update: telegram.Update) -> bool:
+        self.__cur.execute(f'SELECT user_id FROM user_data WHERE user_id == "{update.message.from_user.id}"')
+        return bool(self.__cur.fetchone())
+
+    def create_user_record(self, update: telegram.Update):
+        with self.__conn:
+            self.__cur.execute(f'INSERT INTO user_data (user_id) VALUES ({update.message.from_user.id})')
+
+    def add_learned_words(self, learned_words: List[str], update: telegram.Update):
+        with self.__conn:
+            if not self.find_user_id_record(update=update):
+                self.create_user_record(update=update)
+            self.__cur.execute(f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
+            if self.__cur.fetchone()[0] is None:
+                self.__cur.execute(f'UPDATE user_data SET learned_words = '
+                                   f'"{" ".join(learned_words)}" WHERE user_id = {update.message.from_user.id}')
+            self.__cur.execute(f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
+            learned_words_set = set(self.__cur.fetchone()[0].split() + learned_words)
+            self.__cur.execute(f'UPDATE user_data SET learned_words = '
+                               f'"{" ".join(learned_words_set)}" WHERE user_id = {update.message.from_user.id}')
+
+    def update_best_score(self, score: int, update: telegram.Update):
+        with self.__conn:
+            if not self.find_user_id_record(update=update):
+                self.create_user_record(update=update)
+            self.__cur.execute(
+                f'UPDATE user_data SET best_score = {score} WHERE user_id == {update.message.from_user.id}')
+
+    def get_learned_words(self, update: telegram.Update) -> List[str]:
+        with self.__conn:
+            if not self.find_user_id_record(update=update):
+                self.create_user_record(update=update)
+            self.__cur.execute(f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
+            if self.__cur.fetchone()[0] is not None:
+                self.__cur.execute(
+                    f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
+                return self.__cur.fetchone()[0].split()
+
+    def get_best_score(self, update: telegram.Update) -> int:
+        with self.__conn:
+            if not self.find_user_id_record(update=update):
+                self.create_user_record(update=update)
+            self.__cur.execute(f'SELECT best_score FROM user_data WHERE user_id == {update.message.from_user.id}')
+            return self.__cur.fetchone()[0]
 
 
-def db_add_learned_words(learned_words: List[str], update: telegram.Update):
-    with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
-        cur = conn.cursor()
-        cur.execute(f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
-        if cur.fetchone()[0] is None:
-            cur.execute(f'UPDATE user_data SET learned_words = '
-                        f'"{" ".join(learned_words)}" WHERE user_id = {update.message.from_user.id}')
-        else:
-            cur.execute(f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
-            learned_words_set = set(cur.fetchone()[0].split() + learned_words)
-            cur.execute(f'UPDATE user_data SET learned_words = '
-                        f'"{" ".join(learned_words_set)}" WHERE user_id = {update.message.from_user.id}')
-
-
-def db_update_best_score(score: int, update: telegram.Update):
-    with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
-        cur = conn.cursor()
-        cur.execute(f'UPDATE user_data SET best_score = {score} WHERE user_id == {update.message.from_user.id}')
-
-
-def db_get_learned_words(update: telegram.Update) -> List[str]:
-    with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
-        db_create_user_info(update=update)
-        cur = conn.cursor()
-        cur.execute(f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
-        if cur.fetchone()[0] is not None:
-            cur.execute(f'SELECT learned_words FROM user_data WHERE user_id == {update.message.from_user.id}')
-            return cur.fetchone()[0].split()
-
-
-def db_get_best_score(update: telegram.Update) -> int:
-    with sqlite3.connect('D:\\Start Menu\\Programming\\Keira-Bot\\connectors\\data\\data.db') as conn:
-        cur = conn.cursor()
-        cur.execute(f'SELECT best_score FROM user_data WHERE user_id == {update.message.from_user.id}')
-        return cur.fetchone()[0]
+data_base = DataBase(route=r'D:\Start Menu\Programming\Keira-Bot\connectors\data\data.db')
