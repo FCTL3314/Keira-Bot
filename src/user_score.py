4863ef2225182f4c5ca694b98e5e94e6335b9237
Nikita <Solovev.nikita.05@gmail.com>
2022-10-07 22:26:02 +0300

    Transfer user_score module.

---
+++ src/user_score.py
@@ -0,0 +1,15 @@
+class UserScore:
+    def __init__(self):
+        self._score = 0
+
+    def increment(self):
+        self._score += 1
+
+    def decrement(self):
+        self._score -= 1
+
+    def reset(self):
+        self._score = 0
+
+    def get_score(self):
+        return self._score
