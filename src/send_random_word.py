0c030d8779363dbedd5d23d8b8cb1c2e94b9c1f2
Nikita <Solovev.nikita.05@gmail.com>
2022-10-08 21:53:19 +0300

    readability, simplification

--- src/send_random_word.py
+++ src/send_random_word.py
@@ -8,9 +8,9 @@ async def send_random_word(message: aiogram.types.Message, number_of_words=confi
     """Generates random word and send it"""
     ran_num = random.randint(0, number_of_words - 1)
     await generate_non_previous_number(ran_num=ran_num, message=message)
-    await message.answer(
-        text=f'{create_bot.user_data[f"learning_words: {message.from_user.id}"][create_bot.bot_data[f"ran_num: {message.from_user.id}"]]}',
-        disable_notification=True)
+    learning_words = create_bot.user_data[f"learning_words: {message.from_user.id}"]
+    ran_num = create_bot.bot_data[f"ran_num: {message.from_user.id}"]
+    await message.answer(text=f'{learning_words[ran_num]}', disable_notification=True)
 
 
 async def generate_non_previous_number(ran_num, message: aiogram.types.Message,
