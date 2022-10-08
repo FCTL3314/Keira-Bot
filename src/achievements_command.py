345339b3edc267404eb087d99205ddeed8cb6904
Nikita <Solovev.nikita.05@gmail.com>
2022-10-08 13:09:34 +0300

    disable notification from all messages

--- src/achievements_command.py
+++ src/achievements_command.py
@@ -6,14 +6,19 @@ async def send_achievements_message(message: aiogram.types.Message):
     learned_words = connectors.db_actions.data_base.get_learned_words(message=message)
     best_score = connectors.db_actions.data_base.get_best_score(message=message)
     if learned_words is None:
-        await message.answer(text=f'Что-то я не могу найти слов в твоей библиотеке...')
+        await message.answer(text=f'Что-то я не могу найти слов в твоей библиотеке...',
+                             disable_notification=True)
         if best_score == 0:
-            await message.answer(text=f'Твой лучший счёт так же пуст.')
+            await message.answer(text=f'Твой лучший счёт так же пуст.',
+                                 disable_notification=True)
         else:
-            await message.answer(text=f'Однако твой лучший счёт - {best_score}')
+            await message.answer(text=f'Однако твой лучший счёт - {best_score}',
+                                 disable_notification=True)
     else:
-        await message.answer(text=f'Библиотека твоих слов:\n{", ".join(learned_words)}.')
-        await message.answer(text=f'Твой лучший счёт - {best_score}')
+        await message.answer(text=f'Библиотека твоих слов:\n{", ".join(learned_words)}.',
+                             disable_notification=True)
+        await message.answer(text=f'Твой лучший счёт - {best_score}',
+                             disable_notification=True)
 
 
 def register_achievements_command_handlers(dp: aiogram.Dispatcher):
