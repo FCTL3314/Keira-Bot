2c42700d8f6806ff96ed591cb63f892c291a0504
Nikita <Solovev.nikita.05@gmail.com>
2022-10-09 15:47:42 +0300

    func rename

--- handlers/check_translated_word_correctness.py
+++ handlers/check_translated_word_correctness.py
@@ -3,7 +3,7 @@ import utils
 import states
 
 
-async def check_answer_correctness(message: aiogram.types.Message):
+async def check_translated_word_correctness(message: aiogram.types.Message):
     """Checks the translated word entered by the user for correctness"""
     if message.text.lower() == utils.misc.other.get_random_translated_word(message=message).lower():
         await utils.misc.other.correct_answer_response(message=message)
@@ -12,6 +12,6 @@ async def check_answer_correctness(message: aiogram.types.Message):
 
 
 def register_check_answer_correctness(dp: aiogram.Dispatcher):
-    dp.register_message_handler(check_answer_correctness, content_types=['text'],
+    dp.register_message_handler(check_translated_word_correctness, content_types=['text'],
                                 state=states.begin_learn_words_steps.BeginLearnWordsSteps.
                                 check_answer_correctness_state)
