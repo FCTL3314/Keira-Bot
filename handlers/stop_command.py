f19eeb58f45ff1cf6eb9c4c5957f3df2789e01f8
Nikita <Solovev.nikita.05@gmail.com>
2022-10-09 16:03:49 +0300

    move send_unable_execute_stop_command_message from utils.send_message

--- handlers/stop_command.py
+++ handlers/stop_command.py
@@ -1,10 +1,9 @@
 import aiogram
 import data
 import states
-import utils
 
 
-async def stop_translate(message: aiogram.types.Message, state: aiogram.dispatcher.FSMContext):
+async def send_stop_translating_message(message: aiogram.types.Message, state: aiogram.dispatcher.FSMContext):
     await message.answer(text='Поняла, останавливаюсь...\nНапиши /set если желаешь начать заново.',
                          reply_markup=aiogram.types.reply_keyboard.ReplyKeyboardRemove(),
                          disable_notification=True)
@@ -12,8 +11,12 @@ async def stop_translate(message: aiogram.types.Message, state: aiogram.dispatch
     await state.finish()
 
 
+async def send_unable_execute_stop_command_message(message: aiogram.types.Message):
+    await message.answer(text='Мне нечего останавливать. Напиши /set для начала.', disable_notification=True)
+
+
 def register_stop_command_handler(dp: aiogram.Dispatcher):
-    dp.register_message_handler(utils.misc.send_message.send_unable_execute_stop_command_message, commands=['stop'])
-    dp.register_message_handler(stop_translate, commands=['stop'],
+    dp.register_message_handler(send_unable_execute_stop_command_message, commands=['stop'])
+    dp.register_message_handler(send_stop_translating_message, commands=['stop'],
                                 state=states.begin_learn_words_steps.BeginLearnWordsSteps.
                                 check_answer_correctness_state)
