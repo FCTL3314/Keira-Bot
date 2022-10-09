6447015448874f8d87c39e2541fc0c57ebf0d030
Nikita <Solovev.nikita.05@gmail.com>
2022-10-09 15:38:41 +0300

    added check_for_punctuation function

--- filters/validate_words.py
+++ filters/validate_words.py
@@ -3,6 +3,7 @@ import data
 import utils
 
 from typing import List
+from string import punctuation
 
 
 async def validate_words(learning_words: List[str], message: aiogram.types.Message,
@@ -11,7 +12,9 @@ async def validate_words(learning_words: List[str], message: aiogram.types.Messa
     Validate learning_words for correctness.
     :return: number 1 for conversation handler state.
     """
-    if not await check_for_numbers(learning_words=learning_words):
+    if not await check_for_punctuation(learning_words=''.join(learning_words)):
+        await utils.misc.send_message.send_words_not_accepted_message(message=message, cause='WordsContainPunctuation')
+    elif not await check_for_numbers(learning_words=''.join(learning_words)):
         await utils.misc.send_message.send_words_not_accepted_message(message=message, cause='WordsContainNumbers')
     elif not len(learning_words) == number_of_words:
         await utils.misc.send_message.send_words_not_accepted_message(message=message, cause='InvalidNumberOfWords')
@@ -20,8 +23,15 @@ async def validate_words(learning_words: List[str], message: aiogram.types.Messa
         return True
 
 
-async def check_for_numbers(learning_words: List[str]) -> bool:
-    for symbol in str(learning_words):
+async def check_for_punctuation(learning_words: str) -> bool:
+    for symbol in learning_words:
+        if symbol in punctuation:
+            return False
+    return True
+
+
+async def check_for_numbers(learning_words: str) -> bool:
+    for symbol in learning_words:
         if symbol.isdigit():
             return False
     return True
