e47402d85f767e7b6f2c98577668ee33ea3f1cb7
Nikita <Solovev.nikita.05@gmail.com>
2022-10-10 19:45:32 +0300

    on_startup function call

--- main.py
+++ main.py
@@ -6,18 +6,15 @@ from loader import dp
 
 
 def main():
-    print('*START*')
     handlers.start_command.register_start_command_handlers(dp=dp)
     handlers.set_command.register_set_command_handlers(dp=dp)
     handlers.get_learning_words.register_get_learning_words_handlers(dp=dp)
     handlers.stop_command.register_stop_command_handler(dp=dp)
     handlers.achievements_command.register_achievements_command_handlers(dp=dp)
     handlers.check_translated_word_correctness.register_check_translated_word_correctness_handlers(dp=dp)
-    handlers.unexpected_messages_reply.register_unexpected_message_handlers(dp=dp)
+    handlers.unexpected_messages.register_unexpected_message_handlers(dp=dp)
 
-    aiogram.executor.start_polling(dispatcher=dp,
-                                   on_startup=utils.sql.db_actions.data_base.create_table_if_not_exists(),
-                                   skip_updates=True)
+    aiogram.executor.start_polling(dispatcher=dp, on_startup=utils.on_startup(), skip_updates=True)
 
 
 if __name__ == '__main__':
