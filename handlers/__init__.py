9122c01be2e0cfffb928425fe3a98eb0c6cd2ee1
Nikita <Solovev.nikita.05@gmail.com>
2022-10-10 19:47:12 +0300

    import rename

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,4 +4,4 @@ import handlers.get_learning_words
 import handlers.set_command
 import handlers.start_command
 import handlers.stop_command
-import handlers.unexpected_messages_reply
+import handlers.unexpected_messages
