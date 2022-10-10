7a054e4ea689aff9790155e8b540a649abecc076
Nikita <Solovev.nikita.05@gmail.com>
2022-10-10 19:48:40 +0300

    parameter rename

--- keyboards/default/create_keyboard_markup.py
+++ keyboards/default/create_keyboard_markup.py
@@ -1,6 +1,5 @@
 import aiogram
 
 
-def create_keyboard_markup(translated_words):
-    return aiogram.types.reply_keyboard.ReplyKeyboardMarkup(
-        [[translated_words[i]] for i in range(len(translated_words))])
+def create_keyboard_markup(text):
+    return aiogram.types.reply_keyboard.ReplyKeyboardMarkup([[text[i]] for i in range(len(text))])
