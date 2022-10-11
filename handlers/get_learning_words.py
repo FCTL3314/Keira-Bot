b952c144c2cf875833567aab9805890cd0b5916a
Nikita <Solovev.nikita.05@gmail.com>
2022-10-11 23:23:00 +0300

    context manager instead of variable, add create_learning_words_list function and fix bug with non-preservation learning_words in states memory

--- handlers/get_learning_words.py
+++ handlers/get_learning_words.py
@@ -6,15 +6,21 @@ import states
 
 async def get_learning_words(message: aiogram.types.Message, state: aiogram.dispatcher.FSMContext):
     """Gets user-entered words and writes is to context dict."""
-    await state.update_data(learning_words=[word.capitalize() for word in message.text.split()])
-    user_data = await state.get_data()
-    if await filters.validate_words.validate_words(learning_words=user_data.get('learning_words'),
-                                                   message=message,
-                                                   state=state):
+    async with state.proxy() as user_data:
+        user_data['learning_words'] = await create_learning_words_list(text=message.text, state=state)
+        learning_words = user_data['learning_words']
+    print(learning_words)
+    if await filters.validate_words.validate_words(learning_words=learning_words, message=message, state=state):
         utils.misc.console_display_user_words(username=message.from_user.username,
                                               first_name=message.from_user.first_name,
-                                              learning_words=user_data.get('learning_words'))
+                                              learning_words=learning_words)
         await states.learn_words_steps.LearnWordsSteps.next()
+
+
+async def create_learning_words_list(text, state: aiogram.dispatcher.FSMContext):
+    async with state.proxy() as user_data:
+        user_data['learning_words'] = [word.capitalize() for word in text.split()]
+    return user_data['learning_words']
 
 
 def register_get_learning_words_handlers(dp: aiogram.Dispatcher):
