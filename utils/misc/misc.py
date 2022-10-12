a82029252aa9a017054d0530e00eaf5e409b111a
Nikita <Solovev.nikita.05@gmail.com>
2022-10-12 22:50:33 +0300

    send new record message when user broke the record

--- utils/misc/misc.py
+++ utils/misc/misc.py
@@ -1,5 +1,6 @@
 import aiogram
 import data
+import filters
 import utils
 import translators
 import random
@@ -42,9 +43,10 @@ async def correct_answer_response(message: aiogram.types.Message, state: aiogram
     with utils.sql.database as db:
         if user_score.get_score() > db.get_best_score(user_id=message.from_user.id):
             db.update_best_score(score=user_score.get_score(), user_id=message.from_user.id)
+            if not await filters.is_record_message_send(user_score=user_score, state=state):
+                await utils.misc.send_message.send_score_record_message(message=message)
         if user_score.get_score() == 20:
-            db.add_learned_words(learned_words=learning_words,
-                                 user_id=message.from_user.id)
+            db.add_learned_words(learned_words=learning_words,  user_id=message.from_user.id)
     await utils.misc.send_message.send_correct_answer_message(user_score=user_score, message=message)
     await utils.misc.send_message.send_random_word_message(message=message, state=state)
 
