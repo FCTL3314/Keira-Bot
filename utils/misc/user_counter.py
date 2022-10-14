8e5c5eb4b39d89b55b2f092b710d19b55c15baba
Nikita <Solovev.nikita.05@gmail.com>
2022-10-14 23:12:46 +0300

    rename class and module, move create_score_instance func to misc module

---
+++ utils/misc/user_counter.py
@@ -0,0 +1,15 @@
+class UserCounter:
+    def __init__(self):
+        self._score = 0
+
+    def get_score(self):
+        return self._score
+
+    def increment(self):
+        self._score += 1
+
+    def decrement(self):
+        self._score -= 1
+
+    def reset(self):
+        self._score = 0
