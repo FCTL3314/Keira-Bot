c09d2938622a5127030970efc7f3e9d44837c35d
Nikita <Solovev.nikita.05@gmail.com>
2022-10-15 20:06:03 +0300

    webhook instead of polling

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-import aiogram
+import aiogram.utils.executor
 import handlers
 import utils
 
 from loader import dp
+from data.config import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 
-
-def main():
-    handlers.start_command.register_start_command_handlers(dp=dp)
-    handlers.set_command.register_set_command_handlers(dp=dp)
-    handlers.get_learning_words.register_get_learning_words_handlers(dp=dp)
-    handlers.stop_command.register_stop_command_handler(dp=dp)
-    handlers.library_command.register_library_command_handlers(dp=dp)
-    handlers.check_translated_word_correctness.register_check_translated_word_correctness_handlers(dp=dp)
-    handlers.unexpected_messages.register_unexpected_message_handlers(dp=dp)
-
-    aiogram.executor.start_polling(dispatcher=dp, on_startup=utils.on_startup(), skip_updates=True)
+handlers.start_command.register_start_command_handlers(dp=dp)
+handlers.set_command.register_set_command_handlers(dp=dp)
+handlers.get_learning_words.register_get_learning_words_handlers(dp=dp)
+handlers.stop_command.register_stop_command_handler(dp=dp)
+handlers.library_command.register_library_command_handlers(dp=dp)
+handlers.check_translated_word_correctness.register_check_translated_word_correctness_handlers(dp=dp)
+handlers.unexpected_messages.register_unexpected_message_handlers(dp=dp)
 
 
 if __name__ == '__main__':
-    main()
+    aiogram.utils.executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=utils.on_startup,
+        on_shutdown=utils.on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT
+    )
