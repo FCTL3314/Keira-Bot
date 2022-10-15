996bf1a3230d81d4dff3bb6b74d67dccf0c877c8
Nikita <Solovev.nikita.05@gmail.com>
2022-10-15 20:06:40 +0300

    on startup function now set webhook

--- utils/on_startup.py
+++ utils/on_startup.py
@@ -1,7 +1,11 @@
 import utils.sql
 
+from loader import bot
+from data.config import WEBHOOK_URL
 
-def on_startup():
+
+async def on_startup(dp):
     print('*START*')
+    await bot.set_webhook(WEBHOOK_URL)
     with utils.sql.database as db:
         db.create_table()
