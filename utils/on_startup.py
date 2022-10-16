d6a3d9c7906d01bab7101ff7f32e2cc0dcfd9e9b
Nikita <Solovev.nikita.05@gmail.com>
2022-10-16 12:20:31 +0300

    command menu creates on bot startup

--- utils/on_startup.py
+++ utils/on_startup.py
@@ -1,3 +1,4 @@
+import aiogram
 import utils.sql
 
 from loader import bot
@@ -5,7 +6,12 @@ from data.config import WEBHOOK_URL
 
 
 async def on_startup(dp):
-    print('*START*')
+    await bot.set_my_commands([
+        aiogram.types.BotCommand('start', 'Начальная информация.'),
+        aiogram.types.BotCommand('set', 'Указать изучаемые слова.'),
+        aiogram.types.BotCommand('library', 'Библиотека твоих слов.'),
+        aiogram.types.BotCommand('stop', 'Перестать переводить.')
+    ])
     await bot.set_webhook(WEBHOOK_URL)
     with utils.sql.database as db:
         db.create_table()
