bb58f0077bf165596b42139424b97cee0088690c
Nikita <Solovev.nikita.05@gmail.com>
2022-10-21 18:57:11 +0300

    added is_contains_learned_words filter

--- filters/validate_words.py
+++ filters/validate_words.py
@@ -6,7 +6,7 @@ from typing import List
 from string import punctuation
 
 
-async def validate_words(learning_words: List[str], message: aiogram.types.Message,
+async def validate_words(learning_words: List[str], user_id, message: aiogram.types.Message,
                          number_of_words=data.config.NUMBER_OF_WORDS) -> bool:
     """
     Validate learning_words for correctness.
@@ -20,7 +20,7 @@ async def validate_words(learning_words: List[str], message: aiogram.types.Messa
         await utils.misc.send_message.send_words_not_accepted_message(learning_words=learning_words,
                                                                       cause='WordsContainNumbers',
                                                                       message=message)
-    elif await is_repeated(learning_words=learning_words):
+    elif await is_contains_repeated_words(learning_words=learning_words):
         await utils.misc.send_message.send_words_not_accepted_message(learning_words=learning_words,
                                                                       cause='WordsRepeated',
                                                                       message=message)
@@ -28,6 +28,9 @@ async def validate_words(learning_words: List[str], message: aiogram.types.Messa
         await utils.misc.send_message.send_words_not_accepted_message(learning_words=learning_words,
                                                                       cause='InvalidNumberOfWords',
                                                                       message=message)
+    elif await is_contains_learned_words(learning_words=learning_words, user_id=user_id):
+        await utils.misc.send_message.send_words_contains_learned_words_message(message=message)
+        return True
     else:
         return True
 
@@ -46,10 +49,19 @@ async def is_contains_numbers(learning_words: str) -> bool:
     return False
 
 
-async def is_repeated(learning_words: List[str]) -> bool:
-    words = dict.fromkeys(learning_words, 1)
+async def is_contains_repeated_words(learning_words: List[str]) -> bool:
+    words = dict.fromkeys(learning_words, 0)
     for word in learning_words:
         words[word] += 1
         if words[word] > 1:
+            return True
+    return False
+
+
+async def is_contains_learned_words(learning_words: List[str], user_id) -> bool:
+    with utils.database.postgres_database as db:
+        learned_words = db.get_learned_words(user_id=user_id)
+    for word in learning_words:
+        if word in learned_words:
             return True
     return False
