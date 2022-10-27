3831b486022aaecfa1ebf5fe513023a2f0161d03
Nikita <Solovev.nikita.05@gmail.com>
2022-10-27 16:28:21 +0300

    added user medals message

--- handlers/library_command.py
+++ handlers/library_command.py
@@ -4,13 +4,21 @@ import states
 
 
 async def send_library_message(message: aiogram.types.Message):
+    user_id = message.from_user.id
+    medals_text = await utils.misc.create_medals_text(message=message)
     with utils.database.postgres_database as db:
-        learned_words = db.get_learned_words(user_id=message.from_user.id)
+        learned_words = db.get_learned_words(user_id=user_id)
     if learned_words is None:
         await message.answer(text=f'❕Что-то я не могу найти слов в твоей библиотеке...',
                              disable_notification=True)
     else:
         await message.answer(text=f'📜Библиотека твоих слов:\n{", ".join(sorted(learned_words))}.',
+                             disable_notification=True)
+    if medals_text:
+        await message.answer(text=f'🏆Витрина твоих медалей:\n{medals_text}.',
+                             disable_notification=True)
+    else:
+        await message.answer(text=f'❕Видимо у тебя ещё нет медалей...',
                              disable_notification=True)
 
 
