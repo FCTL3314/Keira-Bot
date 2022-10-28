b661cf5a75a3242db517706af5f76cd3e48d5cf3
Nikita <Solovev.nikita.05@gmail.com>
2022-10-28 22:16:39 +0300

    combine messages and edit their text

--- handlers/start_command.py
+++ handlers/start_command.py
@@ -1,26 +1,23 @@
 import aiogram
-
-from loader import bot
+import utils
 
 
 async def start_command(message: aiogram.types.Message):
     """Send a message that informs the user"""
-    get_bot = await bot.get_me()
-    await message.answer(
-        text=f'Привет {message.from_user.first_name}! Меня зовут {get_bot.first_name}.',
-        disable_notification=True
-    )
-    await message.answer(text=f'Я буду помогать тебе изучать иностранные слова.\n'
-                              f'Сперва напиши команду /set, что бы установить их.\n'
-                              f'Язык написанных тобою слов определяется автоматически.',
-                         disable_notification=True)
-    await message.answer(text=f'Слова, которые ты выучил, добавляются в твою библиотеку.\n'
-                              f'Для того что бы выучить слова, тебе необходимо их верно переводить до тех пор,'
-                              f' пока не появится соответствующее сообщение.',
+    user_id = message.from_user.id
+    await message.answer(text=f'Привет {message.from_user.first_name}! Меня зовут Кейра.\n'
+                              'Я буду помогать тебе изучать английские слова. '
+                              'Переводя их, они постепенно будут учиться, пока не станут выученными. '
+                              'Выученные слова, добавляются в твою библиотеку.'
+                              'Вдобавок, я буду награждать тебя различными достижениями.',
                          disable_notification=True)
     await message.answer(
-        text=f'Написав команду /library, ты увидишь библиотеку выученных слов, а так же свой лучший счёт.',
+        text=f'Написав команду /progress, ты увидишь библиотеку выученных слов, а так же витрину своих достижений.',
         disable_notification=True)
+    with utils.database.postgres_database as db:
+        if not db.get_pioneer_achievement(user_id=user_id):
+            db.set_pioneer_achievement(user_id=user_id)
+            await utils.misc.send_message.send_pioneer_achievement_received_message(message=message)
 
 
 def register_start_command_handlers(dp: aiogram.Dispatcher):
