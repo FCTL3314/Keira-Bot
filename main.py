b6dbbf5a3ccad0317eba28236adb508729040dfe
Nikita <Solovev.nikita.05@gmail.com>
2022-10-28 22:18:48 +0300

    function rename

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ handlers.start_command.register_start_command_handlers(dp=dp)
 handlers.set_command.register_set_command_handlers(dp=dp)
 handlers.get_learning_words.register_get_learning_words_handlers(dp=dp)
 handlers.stop_command.register_stop_command_handler(dp=dp)
-handlers.library_command.register_library_command_handlers(dp=dp)
+handlers.progress_command.register_progress_command_handlers(dp=dp)
 handlers.check_translated_word_correctness.register_check_translated_word_correctness_handlers(dp=dp)
 handlers.unexpected_messages.register_unexpected_message_handlers(dp=dp)
 
