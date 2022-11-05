d0c6ad70a2d929914d5e90d8baec06c2632eb968
Nikita <Solovev.nikita.05@gmail.com>
2022-11-05 14:19:30 +0300

    message text edit

--- handlers/progress_command.py
+++ handlers/progress_command.py
@@ -1,6 +1,5 @@
 import aiogram
 import utils
-import states
 
 
 async def send_progress_message(message: aiogram.types.Message):
@@ -9,13 +8,15 @@ async def send_progress_message(message: aiogram.types.Message):
     with utils.database.postgres_database as db:
         learned_words = await db.get_learned_words(user_id=user_id)
     if learned_words is None:
-        await message.answer(text=f'🕸Пытаясь найти твои слова, я сделала вывод, что их нет.',
+        await message.answer(text=f'📜Библиотека твоих слов:\n\n'
+                                  f'🕸Пытаясь найти твои слова, я сделала вывод, что их нет.',
                              disable_notification=True)
     else:
-        await message.answer(text=f'📜Библиотека твоих слов:\n{", ".join(sorted(learned_words))}.',
+        await message.answer(text=f'📜Библиотека твоих слов:\n\n{", ".join(sorted(learned_words))}.',
                              disable_notification=True)
     if not achievements_text:
-        await message.answer(text=f'🕸Видимо, в настоящее время твоя витрина достижений пустует.',
+        await message.answer(text=f'🏵Витрина твоих достижений:\n\n'
+                                  f'🕸Видимо, в настоящее время твоя витрина достижений пустует.',
                              disable_notification=True)
     else:
         await message.answer(text=f'🏵Витрина твоих достижений:\n\n● {achievements_text}',
@@ -24,5 +25,3 @@ async def send_progress_message(message: aiogram.types.Message):
 
 def register_progress_command_handlers(dp: aiogram.Dispatcher):
     dp.register_message_handler(callback=send_progress_message, commands=['progress'])
-    dp.register_message_handler(callback=send_progress_message, commands=['progress'],
-                                state=states.learn_words_steps.LearnWordsSteps.check_answer_correctness_state)
