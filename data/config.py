63fd99c2c8622f095aa7d30ec3e8534777674e96
Nikita <Solovev.nikita.05@gmail.com>
2022-11-20 19:52:19 +0300

    universal config

--- data/config.py
+++ data/config.py
@@ -3,9 +3,7 @@ import os
 
 TOKEN = os.getenv('TOKEN')
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_HOST = f'https://'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
