8e509c0d5c63f0faf4296e2aa917275daa386b5f
Nikita <Solovev.nikita.05@gmail.com>
2022-11-21 19:39:25 +0300

    logging

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-import aiogram.utils.executor
+import logging
+import aiogram
 import handlers
 import utils
 
 from loader import dp
 from data.config import WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
+
+logging.basicConfig(level=logging.INFO, filename="log.log", filemode='w',
+                    format="%(asctime)s - %(levelname)s - %(message)s")
 
 handlers.start_command.register_start_command_handlers(dp=dp)
 handlers.set_command.register_set_command_handlers(dp=dp)
@@ -14,13 +18,17 @@ handlers.check_translated_word_correctness.register_check_translated_word_correc
 handlers.unexpected_messages.register_unexpected_message_handlers(dp=dp)
 
 
-if __name__ == '__main__':
-    aiogram.utils.executor.start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=utils.on_startup,
-        on_shutdown=utils.on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT
-    )
+if __name__ == "__main__":
+    while True:
+        try:
+            aiogram.executor.start_webhook(
+                dispatcher=dp,
+                webhook_path=WEBHOOK_PATH,
+                on_startup=utils.on_startup,
+                on_shutdown=utils.on_shutdown,
+                skip_updates=True,
+                host=WEBAPP_HOST,
+                port=WEBAPP_PORT
+            )
+        except Exception as e:
+            logging.error(msg=e)
