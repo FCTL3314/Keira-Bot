75be44d9bc42e49c1e5d89c3cab799d15a9f6c9f
Nikita <Solovev.nikita.05@gmail.com>
2022-11-26 16:51:51 +0300

    get_achievement instead of get_certain_achievement, one sql execute instead of many separated

--- utils/database/pg_database.py
+++ utils/database/pg_database.py
@@ -44,38 +44,36 @@ class PostgresDatabase:
 
     async def create_tables(self):
         self.__cur.execute("""
-                CREATE TABLE IF NOT EXISTS user_data (
-                user_id BIGINT PRIMARY KEY,
-                user_name varchar(32)
-                )""")
-        self.__cur.execute("""
-                CREATE TABLE IF NOT EXISTS learned_words (
-                user_id bigint REFERENCES user_data(user_id) ON DELETE CASCADE,
-                word varchar(32)
-                )""")
-        self.__cur.execute("""
-                CREATE TABLE IF NOT EXISTS achievements (
-                user_id bigint REFERENCES user_data(user_id) ON DELETE CASCADE,
-                scrabble_achievement bool DEFAULT FALSE,
-                pioneer_achievement bool DEFAULT FALSE
-                )""")
+        CREATE TABLE IF NOT EXISTS user_data (
+        user_id BIGINT PRIMARY KEY,
+        user_name varchar(32));
+        
+        CREATE TABLE IF NOT EXISTS learned_words (
+        user_id bigint REFERENCES user_data(user_id) ON DELETE CASCADE,
+        word varchar(32));
+        
+        CREATE TABLE IF NOT EXISTS achievements (
+        user_id bigint REFERENCES user_data(user_id) ON DELETE CASCADE,
+        scrabble_achievement bool DEFAULT FALSE,
+        pioneer_achievement bool DEFAULT FALSE)
+        """)
 
     async def is_user_exist(self, user_id: int) -> bool:
         self.__cur.execute(f"""SELECT user_id FROM user_data WHERE user_id = {user_id}""")
         return bool(self.__cur.fetchone())
 
     async def create_user_rows(self, user_id: int):
-        self.__cur.execute(f"""INSERT INTO user_data (user_id) VALUES ({user_id})""")
-        self.__cur.execute(f"""INSERT INTO achievements (user_id) VALUES ({user_id})""")
+        self.__cur.execute(f"""
+        INSERT INTO user_data (user_id) VALUES ({user_id});
+        INSERT INTO achievements (user_id) VALUES ({user_id})
+        """)
 
     async def add_learned_words(self, words: List[str], user_id: int):
         if not await self.is_user_exist(user_id=user_id):
             await self.create_user_rows(user_id=user_id)
         learned_words = await self.get_learned_words(user_id=user_id)
         for word in words:
-            if not learned_words:
-                self.__cur.execute(f"""INSERT INTO learned_words (user_id, word) VALUES ({user_id}, '{word}')""")
-            elif word not in learned_words:
+            if not learned_words or word not in learned_words:
                 self.__cur.execute(f"""INSERT INTO learned_words (user_id, word) VALUES ({user_id}, '{word}')""")
 
     async def get_learned_words(self, user_id: int) -> List[str]:
@@ -87,24 +85,13 @@ class PostgresDatabase:
             result = [word[0] for word in words_tuple]
             return result
 
-    async def set_scrabble_achievement(self, user_id: int, value=True):
+    async def set_achievement(self, user_id: int, achievement: str, value=True):
         if not await self.is_user_exist(user_id=user_id):
             await self.create_user_rows(user_id=user_id)
-        self.__cur.execute(f"""UPDATE achievements SET scrabble_achievement = {value} WHERE user_id = {user_id}""")
+        self.__cur.execute(f"""UPDATE achievements SET {achievement} = {value} WHERE user_id = {user_id}""")
 
-    async def get_scrabble_achievement(self, user_id: int) -> bool:
+    async def get_achievement(self, achievement: str, user_id: int) -> bool:
         if not await self.is_user_exist(user_id=user_id):
             await self.create_user_rows(user_id=user_id)
-        self.__cur.execute(f"""SELECT scrabble_achievement FROM achievements WHERE user_id = {user_id}""")
-        return self.__cur.fetchone()[0]
-
-    async def set_pioneer_achievement(self, user_id: int, value=True):
-        if not await self.is_user_exist(user_id=user_id):
-            await self.create_user_rows(user_id=user_id)
-        self.__cur.execute(f"""UPDATE achievements SET pioneer_achievement = {value} WHERE user_id = {user_id}""")
-
-    async def get_pioneer_achievement(self, user_id: int):
-        if not await self.is_user_exist(user_id=user_id):
-            await self.create_user_rows(user_id=user_id)
-        self.__cur.execute(f"""SELECT pioneer_achievement FROM achievements WHERE user_id = {user_id}""")
+        self.__cur.execute(f"""SELECT {achievement} FROM achievements WHERE user_id = {user_id}""")
         return self.__cur.fetchone()[0]
