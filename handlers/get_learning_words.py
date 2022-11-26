e13949a379a272b418296e6b8fed03ff50a82674
Nikita <Solovev.nikita.05@gmail.com>
2022-11-26 18:04:19 +0300

    change user log str

--- handlers/get_learning_words.py
+++ handlers/get_learning_words.py
@@ -10,8 +10,8 @@ async def get_learning_words(message: aiogram.types.Message, state: aiogram.disp
         user_data['learning_words'] = [word.capitalize() for word in message.text.split()]
         learning_words = user_data['learning_words']
     if await filters.validate_words(learning_words=learning_words, message=message):
-        utils.misc.log_user_words(username=message.from_user.username, first_name=message.from_user.first_name,
-                                  learning_words=learning_words)
+        utils.misc.log_user_enter_words(username=message.from_user.username, first_name=message.from_user.first_name,
+                                        learning_words=learning_words)
         await message.answer(text='⏳*Обработка...*', parse_mode='Markdown', disable_notification=True)
         async with state.proxy() as user_data:
             learning_words_translated = await utils.misc.translate_learning_words(learning_words=learning_words)
